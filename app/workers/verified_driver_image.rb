87f34a75596b4b6fea20c986a129fe91987b90d5
Sandeep Chourey <25004643@mahindra.com>
2019-07-29 15:09:49 +0530

    Remove binding

--- app/workers/verified_driver_image.rb
+++ app/workers/verified_driver_image.rb
@@ -3,7 +3,6 @@ class VerifiedDriverImage
   sidekiq_options :retry => 3, :dead => false
 
   def perform(params)
-    binding.pry
     trip = Trip.find(params["data"]["trip_id"]) if params["data"]["trip_id"].present? and params["data"].present?
     if trip.present? and params["data"]["result"] == 1
       trip.update_attributes(verified_driver_image: true)
