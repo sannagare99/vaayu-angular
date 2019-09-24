92726f11a6f2b9fe6f42824012669b5c1143afd5
Sachin Nagare <nagare.sachin2@mahindra.com>
2019-09-24 16:38:38 +0530

    Added route service

--- myApp/js/controller/routerController.js
+++ myApp/js/controller/routerController.js
@@ -2,8 +2,8 @@ app.controller('routeCtrl', function ($scope, $http, $state,Map,DriverService,Si
 
     $scope.place = {};
     Map.init();
-    
-    SiteService.get(function(res) {
+  
+    SiteService.get().$promise.then(function(res) {
       $scope.sites=res.data.list;
     });
     
@@ -257,8 +257,6 @@ app.controller('routeCtrl', function ($scope, $http, $state,Map,DriverService,Si
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
 
-
-
     
     $scope.guards = [
         {
@@ -277,11 +275,11 @@ app.controller('routeCtrl', function ($scope, $http, $state,Map,DriverService,Si
     $scope.$watch('guards', function(guards) {
         $scope.modelAsJson = angular.toJson(guards, true);
     }, true);
-
-    DriverService.get(function(res) {
+    
+    DriverService.get().$promise.then(function(res) {
       $scope.drivers=res.data.drivers;
-      console.log(res);
     }, function(error) {
+      console.log(error)
       // $scope.drivers=res.data.drivers;
       // alert('i am in error');
     });
