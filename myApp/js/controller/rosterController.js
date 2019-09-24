957b51a050d3b35d70c72abb93183dcf586807df
Sachin Nagare <nagare.sachin2@mahindra.com>
2019-09-24 12:53:25 +0530

    Quick fixes

--- myApp/js/controller/rosterController.js
+++ myApp/js/controller/rosterController.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('rosterCtrl', function($scope,Shift,ShiftService){
+angular.module('app').controller('rosterCtrl', function($scope,ShiftService){
 
     ShiftService.get(function(data) {
         console.log(data);
