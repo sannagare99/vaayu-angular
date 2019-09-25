b88473d4ca8fc8e872c1c4a1ad57aba76bcb4710
Rohit G <rohit.g.gituser@gmail.com>
2019-09-25 19:15:02 +0530

    WIP

--- myApp/js/services/rosterService.js
+++ myApp/js/services/rosterService.js
@@ -1,6 +1,6 @@
 
-angular.module('app').factory('RosterService', ['$resource','BASE_URL_8002',
-function ($resource,BASE_URL_8002) {
+angular.module('app').factory('RosterService', ['$resource','BASE_URL_8002','BASE_URL',
+function ($resource,BASE_URL_8002,BASE_URL) {
     return $resource(BASE_URL_8002+'roasterlist', {}, {
         // query: { method: "GET", isArray: true },
         // create: { method: "POST"},
@@ -9,6 +9,10 @@ function ($resource,BASE_URL_8002) {
         // update: { method: "PUT"}
         addVehicle:{
           url: BASE_URL_8002 + 'addvehicle',
+          method: "POST"
+        },
+        getAllSiteList:{
+          url: BASE_URL + 'getAllSiteList',
           method: "POST"
         }
     });
