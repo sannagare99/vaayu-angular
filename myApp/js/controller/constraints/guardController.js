ead4df46bf6d50482c471cb31de14aa1ff244959
Umar Sayyed <umar.sayyed@fugetroncorp.com>
2019-09-25 19:04:32 +0530

    add guard constraint working

--- myApp/js/controller/constraints/guardController.js
+++ myApp/js/controller/constraints/guardController.js
@@ -8,6 +8,8 @@ angular.
     controller: function GuardController($scope, $http) {
 
       this.siteID = "";
+      $scope.when = ''
+      $scope.event = ''
 
       this.$onInit = () => {
         console.log('onInit called createGuard');
@@ -19,14 +21,23 @@ angular.
       // });
 
       $scope.hasError = function (field, validation) {
+        // console.log($scope.form)
         if (validation) {
           return ($scope.form[field].$dirty && $scope.form[field].$error[validation]) || ($scope.submitted && $scope.form[field].$error[validation]);
         }
         return ($scope.form[field].$dirty && $scope.form[field].$invalid) || ($scope.submitted && $scope.form[field].$invalid);
       };
 
-      $scope.submitGuard = function (isValid) {
+      $scope.submitForm = function (isValid) {
         console.log($scope.$parent.siteID)
+        console.log($scope.for)
+        console.log($scope.when)
+        console.log($scope.event)
+        console.log($scope.from_time)
+        console.log($scope.to_time)
+
+        console.log(moment($scope.to_time))
+
         $scope.submitted = true;
         if ($scope.$parent.siteID == null) {
           alert('Select Site Name');
@@ -43,24 +54,26 @@ angular.
           method: 'POST',
           url: 'http://ec2-13-233-214-215.ap-south-1.compute.amazonaws.com/' + 'constraint/insert',
           headers: {
-            // 'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
             'uid': 'deekshithmech@gmail.com',
             'access_token': '8HP_3YQagGCUoWCXiCR_cg',
             'client': 'DDCqul04WXTRkxBHTH3udA',
           },
           data: { 
-            siteId: $scope.$parent.siteID,
+            siteId: parseInt($scope.$parent.siteID),
             type: 'guard',
-            for : $scope.for,
-            event: $scope.event,
-            when: $scope.when,
-            fromTime: $scope.fromTime,
-            toTime: $scope.toTime,
+            for : $scope.for, //male
+            event: $scope.event, // pick drop
+            when: $scope.when, // first last
+            fromTime: $scope.from_time,
+            toTime: $scope.to_time,
           }
         })
           .then(function (res) {
+            console.log(JSON.stringify(res));
             if (res.data['success']) {
-              
+              alert('record inserted successfully.');
+              $scope.$parent.fetchConstraintList($scope.$parent.siteID);
               console.log(JSON.stringify(res.data))
             } else {
               alert(res.data['message']);
