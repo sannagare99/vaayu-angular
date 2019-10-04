e1c40012ff9f93b2263db777456b2f4315ee13ce
Dhruv Sharma <sharma.dhruv@mahindra.com>
2019-10-04 16:53:27 +0530

    Updated Production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,130 +1,82 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # Code is not reloaded between requests.
-  config.cache_classes = true
-
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
+ 
+  config.cache_classes = false
+  
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::KeyValue.new
   config.logger = ActiveSupport::Logger.new(STDOUT)
   config.log_level = :debug
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  
   config.eager_load = false
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
 
-  # Compress JavaScripts and CSS.
-  #config.assets.js_compressor = :uglifier
-  #config.assets.css_compressor = :sass
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=172800'
+    }
+  else
+    config.action_controller.perform_caching = false
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+    config.cache_store = :null_store
+  end
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
-
-  # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "moove_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  ActionMailer::Base.default from: ENV['GMAIL_USENAME']
+  config.active_support.deprecation = :log
 
+  config.active_record.migration_error = :page_load
+  config.assets.debug = true
+
+  config.assets.quiet = true
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # ActionMailer configs
+  ActionMailer::Base.default :from => ENV['GMAIL_USENAME']
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: ENV['SERVER_URL'] }
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               'moove-dev.herokuapp.com',
+      domain:               'localhost',
       user_name:            ENV['GMAIL_USENAME'],
       password:             ENV['GMAIL_PASSWORD'],
       authentication:       'plain',
       enable_starttls_auto: true
   }
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
-
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
-  #config.logger = GELF::Logger.new(
-   # ENV['GRAYLOG_HOST'],
-   # ENV['GRAYLOG_PORT'],
-   # 'WAN',
-   # { :facility => ENV['GRAYLOG_FACILITY'] }
-  # )
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
   Humanize.configure do |config|
     config.default_locale = :en  # [:en, :fr], default: :en
     config.decimals_as = :digits # [:digits, :number], default: :digits
   end
 
-  # Paperclip configs
   Paperclip.options[:command_path] = "/usr/local/bin/"
+
   config.paperclip_defaults = {
+      :url => ':s3_domain_url',
+      :path => '/:class/:attachment/:id_partition/:style/:filename',
       :storage => :s3,
-      :s3_region => ENV['S3_REGION'],
-      :s3_host_name => ENV['S3_HOST_NAME'],
+      :s3_region => 'ap-south-1',
       :s3_credentials => {
-          :access_key_id => ENV['S3_ACCESS_KEY_ID'],
-          :secret_access_key => ENV['S3_SECRET_KEY'],
-          :bucket => ENV['S3_BUCKET'],
+        :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['S3_SECRET_KEY'],
+        :bucket => ENV['S3_BUCKET'],
       }
   }
-
-  config.action_cable.url = 'ws://#{ENV["SERVER_URL"]}/cable'
-  config.action_cable.allowed_request_origins = ['http://#{ENV["SERVER_URL"]}']
-
-  config.action_mailer.default_url_options = { host: ENV['SERVER_URL'] }
-
-  # WEB SOCKET cable config
-  #config.web_socket_server_url = "wss://#{ENV["SERVER_URL"]}/cable"
-  #config.action_cable.allowed_request_origins = ["https://#{ENV["SERVER_URL"]}", "http://#{ENV["SERVER_URL"]}"]
+  # config.after_initialize do
+  #   Bullet.enable = true
+  #   Bullet.console = true
+  #   Bullet.rails_logger = true
+  #   Bullet.bullet_logger = true
+  #   Bullet.add_footer = true
+  # end
 
 end
